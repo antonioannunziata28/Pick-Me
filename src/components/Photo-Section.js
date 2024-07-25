b134038f70ab1da3bd9ff67cc62a1905c372da1e
Antonio <antonio.annunziata@aol.com>
2024-07-25 15:49:51 +0200

    Create new project NFT shop

---
+++ src/components/Photo-Section.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Photo-Section = () => {
+  return (
+    <div>Photo-Section</div>
+  )
+}
+
+export default Photo-Section
---
+++ src/screen/checkout-screen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Checkout = () => {
+  return (
+    <div>checkout-screen</div>
+  )
+}
+
+export default Checkout;
